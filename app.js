56bb9067df1be187622102a0eeb0913e4b9519ce
Disa <justdisa@gmail.com>
2017-01-05 15:01:56 -0800

    Fixed minor errors of syntax and spacing.

--- app.js
+++ app.js
@@ -61,55 +61,49 @@ if (answer6.toUpperCase() === 'Y' || answer6.toUpperCase() === 'YES') {
   alert('Correct! You bet your ass I am!');
   counter ++;
 } else {
-  alert('Totally wrong. This is why I am here!')
+  alert('Totally wrong. This is why I am here!');
 }
 
 var answer7 = prompt('Ok, good job.  Now let\'s try something different. I love to travel. How many countries do you think I\'ve been to?  Please answer with a number. You get 4 guesses.');
 console.log('The user answered: ' + answer7);
 
 for(var index = 0; index < 3; index++) {
-if (parseInt(answer7) === 18) {
-  alert('Wow, you got it!');
-  counter ++;
-  index = 3;
-
-} else if (answer7 > 18) {
-  var answer7 = prompt('Not that many. Try again.');
-} else if (answer7 < 18) {
-  var answer7 = prompt('More than that. Try again.');
-}
-}
-
-
-  var countries = ['taiwan', 'thailand', 'cambodia', 'costa rica', 'spain', 'italy', 'france', 'switzerland', 'dominican republic', 'argentina', 'brazil', 'peru', 'bolivia', 'uruguay', 'chile'];
-
-  for (var index = 0; index < 5; index++) {
-    var answer8 = prompt('Ok, now that you know I\'ve been to 18 countries, lets see if you can guess one that I\'ve been to outside of North America.  I\'ll give you 6 guesses. Good luck.').toLowerCase();
-    console.log('The user guessed ' + answer8);
-    for (var guess = 0; guess < countries.length; guess++) {
-      console.log('what?');
-      if (countries[guess] === answer8) {
-        console.log('yay!');
-        alert('Wow that is correct. Nice work.');
-        index = 14;
-        counter ++;
-      }
-    }
-    if(index < 14) {
-      alert('Nope. Guess again.');
+  if (parseInt(answer7) === 18) {
+    alert('Wow, you got it!');
+    counter ++;
+    index = 3;
+  } else if (answer7 > 18) {
+    var answer7 = prompt('Not that many. Try again.');
+  } else if (answer7 < 18) {
+    var answer7 = prompt('More than that. Try again.');
   }
 }
-      alert(username + ' You got ' + counter + ' questions correct!');
 
+var countries = ['taiwan', 'thailand', 'cambodia', 'costa rica', 'spain', 'italy', 'france', 'switzerland', 'dominican republic', 'argentina', 'brazil', 'peru', 'bolivia', 'uruguay', 'chile'];
+for (var index = 0; index < 5; index++) {
+  var answer8 = prompt('Ok, now that you know I\'ve been to 18 countries, lets see if you can guess one that I\'ve been to outside of North America.  I\'ll give you 6 guesses. Good luck.').toLowerCase();
+  console.log('The user guessed ' + answer8);
+  for (var guess = 0; guess < countries.length; guess++) {
+    console.log('what?');
+    if (countries[guess] === answer8) {
+      console.log('yay!');
+      alert('Wow that is correct. Nice work.');
+      index = 14;
+      counter ++;
+    }
+  }
+  if(index < 14) {
+    alert('Nope. Guess again.');
+  }
+}
+alert(username + ', you got ' + counter + ' questions correct!');
 
+document.write('<p>' + 'Question 1: Do you think I am over 30 years old? Answer: YES. You answered: ' + answer2.toUpperCase() + '</p>');
 
+document.write('<p>' + 'Question 2: Do you think I am from California? Answer: NO. You answered: ' + answer3.toUpperCase() + '</p>');
 
-document.write("<p>"+"Question 1: Do you think I am over 30 years old? Answer: YES. You answered: " + answer2.toUpperCase()+"</p>");
+document.write('<p>' + 'Question 3: Do you think I am a military vet? Answer: NO. You answered: ' + answer4.toUpperCase() + '</p>');
 
-document.write("<p>"+"Question 2: Do you think I am from California? Answer: NO. You answered: " + answer3.toUpperCase()+"</p>");
+document.write('<p>' + 'Question 4: Do you think I have ever traveled outside of North America? Answer: YES. You answered: ' + answer5.toUpperCase() + '</p>');
 
-document.write("<p>"+"Question 3: Do you think I am a military vet? Answer: NO. You answered: " + answer4.toUpperCase()+"</p>");
-
-document.write("<p>"+"Question 4: Do you think I have ever traveled outside of North America? Answer: YES. You answered: " + answer5.toUpperCase()+"</p>");
-
-document.write("<p>"+"Question 5: Do you think I want to learn code to make money? Answer: YES. You answered: " + answer6.toUpperCase()+"</p>");
+document.write('<p>' + 'Question 5: Do you think I want to learn code to make money? Answer: YES. You answered: ' + answer6.toUpperCase() + '</p>');
